af772effb88c0450b6343144737b467917d745f0
Eduar <educartoons@gmail.com>
2020-01-20 22:09:07 -0500

    Pure Functions Components - React

---
+++ app.js
@@ -0,0 +1,32 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed)
+  ]);
+};
+
+// App
+
+const App = () => {
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt me!'),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'dog',
+      breed: 'Hanavese'
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'bird',
+      breed: 'Cocktail'
+    }),
+    React.createElement(Pet, {
+      name: 'Doink',
+      animal: 'cat',
+      breed: 'Mixed'
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
