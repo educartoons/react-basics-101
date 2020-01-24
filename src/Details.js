7fe764f48ef21d993357742e36ae96e0cb6b9daa
Eduar <educartoons@gmail.com>
2020-01-23 22:32:02 -0500

    Creating Component using Classes

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import pet from '@frontendmasters/pet';
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true
+    };
+  }
+  componentDidMount() {
+    pet.animal(+this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    });
+  }
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading...</h2>;
+    } else {
+      const { animal, name, breed, location, description } = this.state;
+      return (
+        <div className="details">
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt me!</button>
+          <p>{description}</p>
+        </div>
+      );
+    }
+  }
+}
 
 export default Details;
