f62858a84f7c3b6406cbaa1dfa70c7ef197c01e2
educartoons <educartoons@gmail.com>
2020-02-13 22:13:17 -0500

    Adding subscribe methods

--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import Pet from './Pet';
+import store from './store';
 
 const Results = ({ pets }) => {
+  const [flag, setFlag] = useState(false);
+
+  function select(state) {
+    return state.theme;
+  }
+
+  let currentValue;
+  function handleChange() {
+    let previousValue = currentValue;
+    currentValue = select(store.getState());
+
+    if (previousValue !== currentValue) {
+      setFlag(true);
+    }
+  }
+  let unsubscribe = store.subscribe(handleChange);
   return (
     <div>
+      {flag && <p>Hola mundo</p>}
       {pets.length === 0 ? (
         <h2>No Pets found</h2>
       ) : (
@@ -25,4 +44,8 @@ const Results = ({ pets }) => {
   );
 };
 
-export default Results;
+const mapStateToProps = ({ theme }) => ({
+  theme
+});
+
+export default connect(mapStateToProps)(Results);
